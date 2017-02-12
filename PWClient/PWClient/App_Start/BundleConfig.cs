0d4717cae290c47a2b7c7ecb13c7b53f6c3ddf4d
anetegithub <anete.anetes@gmail.com>
2017-02-12 13:37:53 +0500

    init

---
+++ PWClient/PWClient/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PWClient
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                        "~/Client/js/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/js/lib").Include(
+                        "~/Client/js/respond.js",
+                        "~/Client/ts/app.js",
+                        "~/Client/materialize/js/materialize.js"));
+
+
+            bundles.Add(new StyleBundle("~/css/materialize").Include(
+                      "~/Client/materialize/css/materialize.css",
+                      "~/Client/materialize/css/main.css"));
+        }
+    }
+}
---
+++ PWClient/PWClient/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PWClient
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var razorEngine = ViewEngines.Engines.OfType<RazorViewEngine>().First();
+            razorEngine.ViewLocationFormats = razorEngine.ViewLocationFormats.Concat(new string[] 
+                { 
+                    "~/Server/Views/{1}/{0}.cshtml",
+                    "~/Server/Views/{1}/{0}.vbhtml",
+                    "~/Server/Views/Shared/{0}.cshtml",
+                    "~/Server/Views/Shared/{0}.vbhtml"
+                }).ToArray();
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
